2db40a2c1fe89417e5a8b82d9c162e11ec44621f
Prince Velarde <vpcharles31@gmail.com>
2021-08-25 19:42:19 +0800

    Database connection

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
