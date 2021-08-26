c8d4ee4ca870d7899f4dce187581ede39edbf648
Prince Velarde <vpcharles31@gmail.com>
2021-08-26 21:06:58 +0800

    Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route GET api/profile
 // @desc Test Route
 // @access Public
-router.get('/', (req, res) => res.send('Profile Route'));
+//router.get('/', (req, res) => res.send('Profile Route'));
+
+// @route GET api/profile/me
+// @desc Get current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'email', 'password']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
