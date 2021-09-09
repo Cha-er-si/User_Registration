efac24350688c43ba13fe693264e17f76b6c3562
Prince Velarde <vpcharles31@gmail.com>
2021-09-09 11:30:55 +0800

    Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -32,6 +34,7 @@ const App = () => {
               <Route exact path='/' component={Landing} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
             <br />
             <Alert />
